a54ad1f5584a2eebc03e536ccd44d9f01226b393
Harry Leddy <34631808+harL06@users.noreply.github.com>
2026-05-28 22:19:06 +0100

    Update direct_cases tb to be synchronous

--- tb/cocotb/test_direct_cases.py
+++ tb/cocotb/test_direct_cases.py
@@ -1,7 +1,8 @@
 # Fixed tests from Verilog Testbench, manually checked output
 
 import cocotb
-from cocotb.triggers import Timer
+from cocotb.triggers import Timer, RisingEdge, ClockCycles, ReadOnly
+from cocotb.clock import Clock
 
 import reference_model
 
@@ -9,10 +10,30 @@ import reference_model
 @cocotb.test()
 async def fixed_direct_cases(dut):
 
+    # set up a 10ns cycle clock
+    clk = Clock(dut.clk, 10, "ns")
+    clk.start()
+
+    # set reset HIGH for start
+    dut.reset.value = 1
+    dut.input_valid.value = 0
+    dut.message.value = 0
+
+    await ClockCycles(dut.clk, 2) # wait two clock cycles, holding reset HIGH
+
+    dut.reset.value = 0 # disable reset signal
+
     # === Test Case 1 ==== #
     message = 0b0000_0000_11111111_00001111_00000000
     dut.message.value = message
-    await Timer(1, unit="ns") # allow message value to settle
+    dut.input_valid.value = 1
+
+    await RisingEdge(dut.clk) # wait until posedge for input to be sampled
+
+    dut.input_valid.value = 0 # don't read input after prev was sampled
+
+    await RisingEdge(dut.clk) # wait for next posedge, output updated
+    await ReadOnly() # wait for values to settle and sim to enter "read only" stage
 
     value_a = int(dut.value_a.value) #255
     value_b = int(dut.value_b.value) #15
@@ -25,8 +46,16 @@ async def fixed_direct_cases(dut):
     assert reject_reason == 2
 
     # === Test Case 2 ==== #
+    await RisingEdge(dut.clk) # set next message at next rising edge
     dut.message.value = 0b0000_0000_11110000_11110000_00000000
-    await Timer(1, unit="ns") # allow message value to settle
+    dut.input_valid.value = 1
+
+    await RisingEdge(dut.clk) # wait until posedge for input to be sampled
+
+    dut.input_valid.value = 0 # don't read input after prev was sampled
+
+    await RisingEdge(dut.clk) # wait for next posedge, output updated
+    await ReadOnly() # wait for values to settle and sim to enter "read only" stage
 
     value_a = int(dut.value_a.value) #240
     value_b = int(dut.value_b.value) #240
@@ -39,8 +68,16 @@ async def fixed_direct_cases(dut):
     assert reject_reason == 2
 
     # === Test Case 3 ==== #
+    await RisingEdge(dut.clk) # set next message at next rising edge
     dut.message.value = 0b0000_0000_01010101_11001101_00000000
-    await Timer(1, unit="ns") # allow message value to settle
+    dut.input_valid.value = 1
+
+    await RisingEdge(dut.clk) # wait until posedge for input to be sampled
+
+    dut.input_valid.value = 0 # don't read input after prev was sampled
+
+    await RisingEdge(dut.clk) # wait for next posedge, output updated
+    await ReadOnly() # wait for values to settle and sim to enter "read only" stage
 
     value_a = int(dut.value_a.value) #85
     value_b = int(dut.value_b.value) #205
@@ -53,8 +90,16 @@ async def fixed_direct_cases(dut):
     assert reject_reason == 3
 
     # === Test Case 4 ==== #
+    await RisingEdge(dut.clk) # set next message at next rising edge
     dut.message.value = 0b0000_0000_00000101_00000011_00000000
-    await Timer(1, unit="ns") # allow message value to settle
+    dut.input_valid.value = 1
+
+    await RisingEdge(dut.clk) # wait until posedge for input to be sampled
+
+    dut.input_valid.value = 0 # don't read input after prev was sampled
+
+    await RisingEdge(dut.clk) # wait for next posedge, output updated
+    await ReadOnly() # wait for values to settle and sim to enter "read only" stage
 
     value_a = int(dut.value_a.value) #5
     value_b = int(dut.value_b.value) #3
@@ -67,8 +112,16 @@ async def fixed_direct_cases(dut):
     assert reject_reason == 0
 
     # === Test Case 5 ==== #
+    await RisingEdge(dut.clk) # set next message at next rising edge
     dut.message.value = 0b0000_0000_11001000_11001000_00000000
-    await Timer(1, unit="ns") # allow message value to settle
+    dut.input_valid.value = 1
+
+    await RisingEdge(dut.clk) # wait until posedge for input to be sampled
+
+    dut.input_valid.value = 0 # don't read input after prev was sampled
+
+    await RisingEdge(dut.clk) # wait for next posedge, output updated
+    await ReadOnly() # wait for values to settle and sim to enter "read only" stage
 
     value_a = int(dut.value_a.value) #200
     value_b = int(dut.value_b.value) #200
@@ -81,8 +134,16 @@ async def fixed_direct_cases(dut):
     assert reject_reason == 0
 
     # === Test Case 6 ==== #
+    await RisingEdge(dut.clk) # set next message at next rising edge
     dut.message.value = 0b0001_0000_01010101_10101010_00000000
-    await Timer(1, unit="ns") # allow message value to settle
+    dut.input_valid.value = 1
+
+    await RisingEdge(dut.clk) # wait until posedge for input to be sampled
+
+    dut.input_valid.value = 0 # don't read input after prev was sampled
+
+    await RisingEdge(dut.clk) # wait for next posedge, output updated
+    await ReadOnly() # wait for values to settle and sim to enter "read only" stage
 
     value_a = int(dut.value_a.value) #200
     value_b = int(dut.value_b.value) #200
@@ -95,8 +156,16 @@ async def fixed_direct_cases(dut):
     assert reject_reason == 1
 
     # === Test Case 7 ==== #
+    await RisingEdge(dut.clk) # set next message at next rising edge
     dut.message.value = 0b0001_0000_11110000_00001111_00000000
-    await Timer(1, unit="ns") # allow message value to settle
+    dut.input_valid.value = 1
+
+    await RisingEdge(dut.clk) # wait until posedge for input to be sampled
+
+    dut.input_valid.value = 0 # don't read input after prev was sampled
+
+    await RisingEdge(dut.clk) # wait for next posedge, output updated
+    await ReadOnly() # wait for values to settle and sim to enter "read only" stage
 
     value_a = int(dut.value_a.value)
     value_b = int(dut.value_b.value)
