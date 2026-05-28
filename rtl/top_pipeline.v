ebe1a33be0d49cd148b44cf5e5810bb5a0b6d419
Harry Leddy <34631808+harL06@users.noreply.github.com>
2026-05-28 15:39:05 +0100

    Made top-level pipeline synchronous and updated verilog tb to reflect that

--- rtl/top_pipeline.v
+++ rtl/top_pipeline.v
@@ -1,39 +1,85 @@
 `timescale 1ns / 1ps
 
 module top_pipeline(
+        input clk,
+        input reset,
+        input input_valid, // should input be ignored
         input wire [31:0] message,
-        output wire [7:0] value_a,
-        output wire [7:0] value_b,
-        output wire [3:0] reject_reason
+
+        output reg output_valid, // should output be ignored
+        output reg [7:0] value_a,
+        output reg [7:0] value_b,
+        output reg [3:0] reject_reason
     );
+
+    reg [31:0] reg_message;
+    reg reg_message_valid;
+    always @ (posedge clk) begin // synchronous logic
+        // input sampling logic
+        if (reset) begin // reset HIGH
+            reg_message <= 0;
+            reg_message_valid <= 0; // current reg message is invalid
+        end
+        else if (input_valid) begin
+            reg_message <= message; // sample input on clk edge if its valid
+            reg_message_valid <= 1; // reg message is valid
+        end
+        else begin // if input invalid, ignore the input
+            reg_message <= 0;
+            reg_message_valid <= 0;
+        end
+
+        // output sampling logic
+        output_valid <= reg_message_valid; // output valid based on prev message valid
+
+        if (reset) begin // reset HIGH
+            output_valid <= 0; // output invalid during reset
+            value_a <= 0;
+            value_b <= 0;
+            reject_reason <= 0;
+        end
+        else if (reg_message_valid) begin // if current reg message is valid, sample output
+            value_a <= value_a_wire; //outputs will be based on prev cycle input
+            value_b <= value_b_wire;
+            reject_reason <= reject_reason_wire;
+        end
+        else begin //outputs zeroed if reg input is invalid
+            value_a <= 0;
+            value_b <= 0;
+            reject_reason <= 0;
+        end
+    end
 
     // parse diferent vars from the input message
     wire [7:0] input_val_a, input_val_b;
     wire [3:0] message_type;
     message_parser parser (
-        .message(message),
+        .message(reg_message), // input the most recent sampled input
         .value_a(input_val_a),
         .value_b(input_val_b),
         .message_type(message_type)
     );
 
     // pass parsed values into message filter
-    wire message_valid;
+    wire message_accept;
+    wire [3:0] reject_reason_wire;
     message_filter filter (
         .message_type(message_type),
-        .message_valid(message_valid),
-        .reject_reason(reject_reason),
+        .message_valid(message_accept),
+        .reject_reason(reject_reason_wire),
         .value_a(input_val_a),
         .value_b(input_val_b)
     );
 
+    wire [7:0] value_a_wire; // wire values driven by combo logic, will be sampled on clk edge
+    wire [7:0] value_b_wire;
     // set outputs only if the message is valid
     output_formatter out_format (
         .input_val_a(input_val_a),
         .input_val_b(input_val_b),
-        .message_valid(message_valid),
-        .value_a(value_a),
-        .value_b(value_b)
+        .message_valid(message_accept),
+        .value_a(value_a_wire),
+        .value_b(value_b_wire)
     );
 
 endmodule
